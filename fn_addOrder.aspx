60ea369062cecc333e7008f100af38c98730e750
berazumis124 <berazumis@gmail.com>
2017-07-01 21:23:34 +0300

    ExcelPackageExtension

---
+++ fn_addOrder.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="fn_addOrder.aspx.cs" Inherits="POQualityCheck.fn_addOrder" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <div>
+            <asp:Label ID="lbl_orderNo" runat="server" Text="Order No:"></asp:Label>
+        </div>
+        <div>
+            <asp:TextBox ID="txt_orderNo" runat="server"></asp:TextBox>
+        </div>
+        <div>
+            <asp:Label ID="lbl_date" runat="server" Text="Order Date:"></asp:Label>
+        </div>
+        <div>
+            <asp:TextBox ID="txt_date" runat="server"></asp:TextBox>
+        </div>
+        <div>
+            <asp:Label ID="lbl_supplier" runat="server" Text="Supplier:"></asp:Label>
+        </div>
+        <div>
+            <asp:DropDownList ID="drb_supplier" runat="server"></asp:DropDownList>
+        </div>
+        <div>
+            <asp:Label ID="lbl_file" runat="server" Text="Select file to upload: "></asp:Label>
+        </div>
+        <div>
+            <asp:FileUpload ID="fu_upload" runat="server" /><asp:Button ID="btn_upload" runat="server" Text="Upload" />
+        </div>
+        <div>
+            <asp:Button ID="btn_save" runat="server" Text="Save" />
+        </div>
+    </div>
+    </form>
+</body>
+</html>
---
+++ fn_addOrder.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using OfficeOpenXml;
+
+namespace POQualityCheck
+{
+    public partial class fn_addOrder : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack && fu_upload.HasFile)
+            {
+                if (Path.GetExtension(fu_upload.FileName).Equals(".xlsx"))
+                {
+
+                    var excel = new ExcelPackage(fu_upload.FileContent);
+                    var dt = excel.ToDataTable();
+
+                    string constr = ConfigurationManager.ConnectionStrings["connDB"].ConnectionString;
+                    using (var con = new SqlConnection(constr))
+                    {
+                        foreach (DataRow dr in dt.Rows)
+                        {
+                            using (SqlCommand cmd = new SqlCommand("sp_import_order"))
+                            {
+                                cmd.CommandType = CommandType.StoredProcedure;
+                                cmd.Parameters.AddWithValue("@OrderNo", dr["orderNo"].ToString());
+                                cmd.Parameters.AddWithValue("@OrderQty", dr["orderQty"].ToString());
+                                cmd.Parameters.AddWithValue("@ShipmentDate", dr["shipmentDate"].ToString());
+                                cmd.Parameters.AddWithValue("@UnitOfMeasure", dr["unitOfMeasure"].ToString());
+                                cmd.Parameters.AddWithValue("@Customer", dr["customer"].ToString());
+                                cmd.Parameters.AddWithValue("@Spalva", dr["Spalva"].ToString());
+                                cmd.Parameters.AddWithValue("@Spyna", dr["Spyna"].ToString());
+                                cmd.Parameters.AddWithValue("@Vyris", dr["Vyris"].ToString());
+                                cmd.Parameters.AddWithValue("@Plotis", dr["Plotis"].ToString());
+                                cmd.Parameters.AddWithValue("@GaminioTipas", dr["Gaminio_Tipas"].ToString());
+                                cmd.Parameters.AddWithValue("@EXTRA1", dr["EXTRA_1"].ToString());
+                                cmd.Parameters.AddWithValue("@EXTRA2", dr["EXTRA_2"].ToString());
+                                cmd.Parameters.AddWithValue("@EXTRA3", dr["EXTRA_3"].ToString());
+                                cmd.Parameters.AddWithValue("@EXTRA4", dr["EXTRA_4"].ToString());
+                                cmd.Parameters.AddWithValue("@UserID", Session["UserID"].ToString());
+                                cmd.Connection = con;
+                                con.Open();
+                                SqlDataReader reader = cmd.ExecuteReader();
+                                reader.Close();
+                                con.Close();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
